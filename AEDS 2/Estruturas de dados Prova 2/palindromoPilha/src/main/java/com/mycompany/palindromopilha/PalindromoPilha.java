aa44d671cd3c822d23b38e2401d5aa549fdce6f5
Gustavo Delfino Guimarães <delfinoguimaraes1@gmail.com>
2024-10-29 15:06:32 -0300

    update palindromo exercicio

---
+++ AEDS 2/Estruturas de dados Prova 2/palindromoPilha/src/main/java/com/mycompany/palindromopilha/PalindromoPilha.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.palindromopilha;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ *
+ * @author delfi
+ */
+public class PalindromoPilha {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String entrada = sc.nextLine();
+        
+        while(!entrada.equals("FIM")){
+            Pilha<Character> pilha = new Pilha<>();
+            
+            for(int i = 0; i < entrada.length(); i++){
+                pilha.empilhar(entrada.charAt(i));
+            }
+            
+            boolean verifica = palindromo(pilha);
+            
+            if(verifica){
+                System.out.println( entrada + " e palindromo de: " + entrada);
+            } else System.out.println(entrada + " nao e palindromo de: " + entrada);
+            
+            entrada = sc.nextLine();
+        }
+    }
+    
+    public static boolean palindromo(Pilha<Character> pilha){
+        Pilha<Character> aux = new Pilha<Character>();
+        aux = pilha.clone();
+        aux.inverter();
+        aux.desempilhar();
+        int tamanho =  pilha.getTamanho();
+        int cont = 0;
+        
+        while(!aux.vazia()){
+            Character p = pilha.desempilhar();
+            Character a = aux.desempilhar();
+            
+            if(Objects.equals(p, a)){
+                cont++;
+            }
+        }
+       return tamanho == cont;
+    }
+}
